c6449c93b162844ae9a05a15b1ec5b5294eb51df
Hossam555666 <hm261920@gmail.com>
2023-01-17 23:09:53 +0200

    after adding json file

--- js/cards.js
+++ js/cards.js
@@ -1,44 +1,127 @@
-const heartIcone =document.querySelector('.fa-heart')
-const heartNumber =document.querySelector('.heart-number')
-const commentNumber=document.querySelector('.comment-number')
-const shearNumber=document.querySelector('.share-number')
-const stars = document.querySelectorAll('.fa-star')
+const Data =document.querySelector('.row');
+import{DataContainer} from "../js/json.js";
+console.log(DataContainer[1].author)
+showData()
+
+const heartIcones =document.querySelectorAll('.fa-heart')
+const heartNumbers =document.querySelectorAll('.heart-number')
 
 
 
-heartIcone.addEventListener('click',function(e){
 
-  let name =e.target;
-  if(heartNumber.innerHTML=='275'){
-    let number=heartNumber.innerHTML;
-      number++
-      heartNumber.innerHTML=number
-      heartIcone.style.color = 'red'
-  }else {
-    let number=heartNumber.innerHTML;
-      number--
-      heartNumber.innerHTML=number
-      heartIcone.style.color = 'rgba(0, 0, 0, 0.4)'
+
+
+
+// async function getData(){
+//   let myResponse = await fetch('cards.json');
+//   let myData = await myResponse.json();
+//   console.log(myData)
+
+// }
+
+// getData();
+
+
+ function showData(){
+  let divs='';
+  for(let i=0; i<DataContainer.length; i++){
+    console.log(DataContainer)
+    divs +=`
+    <div class="col-lg-6 ">
+        <div class="Data rounded bg-white rounded shadow ">
+          <div class="img-part position-relative   ">
+            <img src="${DataContainer[i].src}" class=" rounded-top " alt="">
+            <button class="btn btn-dark py-0 rounded-pill">${DataContainer[i].name}</button>
+            <i class="fa-solid fa-ellipsis-vertical text-white "></i>
+          </div>
+
+          <div class="p-3">
+            <div class='d-flex '>
+              <span class="iqone${i} ${DataContainer[i].rating}">
+                <i class="fa-regular fa-star opacity-25"></i>
+                <i class="fa-regular fa-star opacity-25"></i>
+                <i class="fa-regular fa-star opacity-25"></i>
+                <i class="fa-regular fa-star opacity-25"></i>
+                <i class="fa-regular fa-star opacity-25 pe-3"></i>
+              </span class='ratingNumber'>
+              <span>${DataContainer[i].date}</span>
+              
+            </div>
+            <h5>${DataContainer[i].h5}</h5>
+            <p>${DataContainer[i].p}</p>
+            <p class="author">${DataContainer[i].author}</p>
+            <hr>
+            <div class="iqone-part ">
+              <i class="fa-solid fa-heart "></i><span class="heart-number me-4 p-1">275</span>
+              <i class="fa-solid fa-message "></i><span class=" me-4  p-1">7</span>
+              <i class="fa-solid fa-share"></i> <span class=" me-4 p-1"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+    
+
+
   }
-});
-
-
-
-
-stars.forEach((s,i) => {s.addEventListener('click',function(){
-  
-  for(let j=0; j<=i; j++){
-    stars[j].classList.replace('fa-regular','fa-solid')
-    stars[j].classList.add('text-warning')
-    stars[j].classList.remove('opacity-25')
-  }
-  for(let k=i+1; k<=stars.length; k++ ){
-  stars[k].classList.replace('fa-solid','fa-regular')
-  stars[k].classList.remove('text-warning')
-  stars[k].classList.add('opacity-25')
+  Data.innerHTML=divs;
 }
-})
+
+
+
+
+
+heartIcones.forEach((heart,i) => {
   
+
+heart.addEventListener('click',function(e){
+  if(heartNumbers[i].innerHTML=='275'){
+    let number=heartNumbers[i].innerHTML;
+      number++
+      heartNumbers[i].innerHTML=number
+      heartIcones[i].style.color = 'red'
+  }else {
+    let number=heartNumbers[i].innerHTML;
+      number--
+      heartNumbers[i].innerHTML=number
+      heartIcones[i].style.color = 'rgba(0, 0, 0, 0.4)'
+  }
+});
+
 });
 
 
+
+// this loop to select which card will use its stars
+for(let n=0; n<DataContainer.length; n++){
+let starsGroup = document.querySelectorAll(`.iqone${n}>.fa-star`)
+
+
+
+
+starsGroup.forEach((s,i) => {s.addEventListener('click',function(){
+
+  starsGroup[i].classList.toggle('text-warning')
+  starsGroup[i].classList.toggle('opacity-25')
+  starsGroup[i].classList.toggle('fa-solid')
+  for(let j=0; j<i; j++){
+    starsGroup[j].classList.add('fa-solid')
+    starsGroup[j].classList.add('text-warning')
+    starsGroup[j].classList.remove('opacity-25')
+  }
+  for(let k=i+1; k<=starsGroup.length; k++ ){
+  starsGroup[k].classList.remove('fa-solid')
+  starsGroup[k].classList.remove('text-warning')
+  starsGroup[k].classList.add('opacity-25')
+}
+
+
+
+})
+
+
+
+  
+});
+
+}
---
+++ js/json.js
@@ -0,0 +1,38 @@
+export let DataContainer=[
+    {
+        src: "image/one.jpg",
+        name: "ARTICLE",
+        rating: "d-none",
+        date: "3/24/2020",
+        h5: "A smartphone photo can spot fake rice at your grocery store",
+        p: "next time you go to the neighbourhood grocery store, take your smartphone",
+        author: "by john smith"
+    },
+    {
+        src: "image/two.png",
+        name: "ARTICLE",
+        rating: "d-block",
+        date: "12",
+        h5: "18 useful metrics for measuring blockchain progress",
+        p: "next time you go to the neighbourhood grocery store, take your smartphone",
+        author: "by john smith"
+    },
+    {
+        src: "image/three.jpg",
+        name: "NEWS",
+        rating: "d-block",
+        date: "12",
+        h5: "18 useful metrics for measuring blockchain progress",
+        p: "next time you go to the neighbourhood grocery store, take your smartphone",
+        author: "by john smith" 
+    },
+    {
+        src: "image/asset 12.jpeg",
+        name: "BIOG",
+        rating: "d-none",
+        date: "3/24/2020",
+        h5: "A smartphone photo can spot fake rice at your grocery store",
+        p: "",
+        author: "" 
+    }
+]
